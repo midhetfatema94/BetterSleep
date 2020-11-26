70a01f946e47b785b88699ee7b3b9dcecd457286
Midhet Sulemani <midhetfatema94@gmail.com>
2020-11-26 18:26:18 +0300

    first commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Waveline Media on 11/26/20.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
